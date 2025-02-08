95cfda743f7021af5c0b5f15c3ddd201fe7d7049
dung195 <nguyentiendung100606@outlook.com.vn>
2025-02-08 23:16:37 +0700

    change

--- temp.cpp
+++ temp.cpp
@@ -1,32 +1,54 @@
 #include <SDL2/SDL.h>
-#include<bits/stdc++.h>
-using namespace std;
-#define int long long
-#define ld long long double
-const int SCREEN_WIDTH=800;
-const int SCREEN_HEIGHT=600;
-SDL_Window* initSDL(){
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
-    if (window == NULL) {
-        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    // Create window
+    SDL_Window* window = SDL_CreateWindow("SDL2 Window",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          800, 600,
+                                          SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
         return 1;
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL) {
-        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
-        return 1;
+    // Main loop flag
+    bool quit = false;
+
+    // Event handler
+    SDL_Event e;
+
+    // While application is running
+    while (!quit) {
+        // Handle events on queue
+        while (SDL_PollEvent(&e) != 0) {
+            // User requests quit
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        // Clear screen
+        SDL_SetRenderDrawColor(SDL_GetRenderer(window), 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(SDL_GetRenderer(window));
+
+        // Update screen
+        SDL_RenderPresent(SDL_GetRenderer(window));
     }
-}
-// them comment 
-int main(){
-    SDL_Window* window=initSDL();
-    SDL_Renderer* renderer=createRenderer();
 
+    // Destroy window
+    SDL_DestroyWindow(window);
 
+    // Quit SDL subsystems
+    SDL_Quit();
+
+    return 0;
 }
